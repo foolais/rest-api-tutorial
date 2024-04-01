a06526f7e5993bd3327d15fc10f41748b309f369
wahyu.esya17 <wahyu.esya17@gmail.com>
2024-04-01 23:14:11 +0700

    feat(Main): add body-parser n cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const routes = require('./routes');
 const logger = require('./utils/logger');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 4000;
+
+// parse body request
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// cors access handler
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+);
 
 routes(app);
 
